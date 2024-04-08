96879745e5de6b5a76d45cb4a981013078604759
Ahsan Usman <ahsanusmanofficial@gmail.com>
2024-04-08 10:50:48 +0500

    complete the delete functionality

--- frontend/src/components/WorkoutCard.jsx
+++ frontend/src/components/WorkoutCard.jsx
@@ -2,17 +2,53 @@ import * as React from "react";
 import Box from "@mui/joy/Box";
 import Card from "@mui/joy/Card";
 import CardContent from "@mui/joy/CardContent";
+import { Button } from "@mui/material";
 import Typography from "@mui/joy/Typography";
-import '../styles/WorkoutCard.css'
-const WorkoutCard = ({workout}) => {
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import "../styles/WorkoutCard.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+const WorkoutCard = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+  const handleDelete = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const jsonResponse = await response.json();
+    if (response.ok) {
+      dispatch({
+        type: "DELETE_WORKOUT",
+        payload: jsonResponse,
+      });
+    }
+  };
   return (
-    <Box className='workoutCard'>
-      <Card variant="outlined" >
-        <CardContent>
-          <Typography  className='workoutCardTitle'>{workout.title}</Typography>
-          <Typography className='workoutCardLoad'>Load (KG): {workout.load}</Typography>
-          <Typography className='workoutCardReps'>Reps: {workout.reps}</Typography>
-          <Typography className='workoutCardCreatedAt'>{workout.createdAt}</Typography>
+    <Box className="workoutCard">
+      <Card variant="outlined">
+        <CardContent className='cardContentMain'>
+          <div className="typographyDiv">
+            <Typography className="workoutCardTitle">
+              {workout.title}
+            </Typography>
+            <Typography className="workoutCardLoad">
+              Load (KG): {workout.load}
+            </Typography>
+            <Typography className="workoutCardReps">
+              Reps: {workout.reps}
+            </Typography>
+            <Typography className="workoutCardCreatedAt">
+              {workout.createdAt}
+            </Typography>
+          </div>
+          <div className="buttonDiv">
+            <Button
+              className="workoutCardButton"
+              onClick={handleDelete}
+            >
+              <FontAwesomeIcon icon={faTrash} style={{ color: 'red' }} />
+            </Button>
+          </div>
         </CardContent>
       </Card>
     </Box>
